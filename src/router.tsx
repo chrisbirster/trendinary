502c02a0dba7694b110b2e2e035d7a2c83edb0f8
Chris Birster <christopher.birster@gmail.com>
2026-08-30 21:49:31 -0400

    Wire Solid Router pages

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { createRouter } from "@solidjs/router";
+import { FomoPage, FollowingPage, HomePage, NotFoundPage, PeepPage, TrendPage } from "./pages";
+
+export const Router = createRouter({
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/peep", component: PeepPage },
+    { path: "/fomo", component: FomoPage },
+    { path: "/following", component: FollowingPage },
+    { path: "/trend/:slug", component: TrendPage },
+    { path: "*404", component: NotFoundPage },
+  ],
+});
+
+export const { paths } = Router;
